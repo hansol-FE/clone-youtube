a65cce43eb395447c9ba56b26206ad971e423afc
hansol-FE <dundunki@naver.com>
2022-07-21 20:26:24 +0900

    fix detail page

--- src/components/video_detail/video_detail.jsx
+++ src/components/video_detail/video_detail.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
 import styles from './video_detail.module.css';
 
-const VideoDtail = ({video}) => {
+const VideoDtail = ({video, video:{snippet}}) => {
   return (
-    <h1>{video.snippet.title}</h1>
+    <section classNmae={styles.detail}>
+      <iframe className={styles.video}
+        title={video.id}
+        type="text/html" 
+        width="100%" 
+        height="500"
+        src={`https://www.youtube.com/embed/${video.id}`}
+        frameborder="0" 
+        allowfullscreen>
+      </iframe>
+      <h2>{snippet.title}</h2>
+      <h3>{snippet.channelTitle}</h3>
+      <pre className={styles.description}>{snippet.description}</pre>
+    </section>
   );
 };
 
